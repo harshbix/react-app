475720631b798d63b0f846dae65169beed8ca5e1
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-19 21:37:47 +0300

    Update ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 const ListGroup = () => {
   let items = ["Uyole", "Posta", "Utukuyu", "Kati"];
-  let selectedIndex = 0;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
@@ -8,11 +10,11 @@ const ListGroup = () => {
         {items.length === 0 && <h2>No item here!</h2>}
         {items.map((item,index) => (
           <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} 
-          onClick={() => selectedIndex = index}>
+          onClick={() => setSelectedIndex(index)}>
             {item}
           </li>
         ))}
-      </ul> 
+      </ul>  
     </>
   );
 };
