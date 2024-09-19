15acc656fcc58391215b81e4ef3665266a0c8ee0
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-19 20:17:02 +0300

    state learning

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 const ListGroup = () => {
   let items = ["Uyole", "Posta", "Utukuyu", "Kati"];
+  let selectedIndex = 0;
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.length === 0 && <h2>No item here!</h2>}
         {items.map((item,index) => (
-          <li className="list-group-item" key={item} 
-          onClick={()=>console.log(item , index)
-          }>
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} 
+          onClick={() => selectedIndex = index}>
             {item}
           </li>
         ))}
