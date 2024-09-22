09493acc9d12b6ec65738ab9a81b8e52dc730d64
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-22 13:42:34 +0300

    make the button and the alert usable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import ButtonComp from './components/ButtonComp'
+import ButtonComp from "./components/ButtonComp";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 const App = () => {
   let text = "Hi mom!";
+ const [alertVisble, setAlertVisible] =  useState(false);
+ const handleClick = () =>
+ {
+    setAlertVisible(true);
+ }
   return (
     <div>
+      {alertVisble && <Alert onClose={() => setAlertVisible(false)}>
+        <span>Alert</span>
+        </Alert>}
       <ButtonComp>
-        <button type="button" className='btn btn-primary'>{text}</button>
-      </ButtonComp></div>
-  )
-}
+        <button type="button" className="btn btn-primary" onClick={handleClick}>
+          {text}
+        </button>
+      </ButtonComp>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,12 +3,15 @@ import { ReactNode } from "react";
 interface Props
 {
     children: ReactNode;
+    onClose: () => void;
 }
 
 
-const Alert = ({children} : Props ) => {
+const Alert = ({children, onClose} : Props ) => {
   return (
-    <div>{children}</div>
+    <div className="alert alert-info alert-dismissible">{children}
+    <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss ="alert"></button>
+    </div>
   )
 }
 
